c7331d0279cd9739b880b61aaf05248e5b08de6b
Yatharth <yatharthvaishnav202@gmail.com>
2026-06-13 11:34:25 +0530

    Automate The Boring Stuff With Python functions

---
+++ ATBS_1 to 3 Combined/digit count digit sum and reversed number.py
@@ -0,0 +1,13 @@
+def operations(n):
+    sum=0
+    reverse=0
+    count=0
+    while True:
+        last=n%10
+        reverse
+        print(n)
+        count+=1
+        if count==100:
+            break
+    return f"done"
+print(operations(11))
---
+++ ATBS_1 to 3 Combined/grade system for 5 subjects.py
@@ -0,0 +1,20 @@
+def grade(a, b, c, d, e):
+    total = a + b + c + d + e
+    average = total / 5
+
+    if average >= 90:
+        return 'A'
+    elif average >= 80:
+        return 'B'
+    elif average >= 70:
+        return 'C'
+    elif average >= 60:
+        return 'D'
+    else:
+        return 'F'
+subject1 = float(input("Enter marks for subject 1: "))
+subject2 = float(input("Enter marks for subject 2: "))
+subject3 = float(input("Enter marks for subject 3: "))
+subject4 = float(input("Enter marks for subject 4: "))
+subject5 = float(input("Enter marks for subject 5: "))
+print("Your grade is: ", grade(subject1, subject2, subject3, subject4, subject5))
